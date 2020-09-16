ec0a567354e73eb8364ffc5869f3276c740eda0e
Dmitrii Celpan <celpand@gmail.com>
2020-09-16 17:02:22 +0300

    Integrate sourcery generation into PhotosAPI and mocks

---
+++ PhotosAPIMocks/Generated/Testing.generated.swift
@@ -0,0 +1,5 @@
+// Generated using Sourcery 1.0.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// Found 18 Types
+//  APIError   CancellableTask   EmptyCancellable   FlickrError   FlickrPhotosAPI   FlickrResponse   FlickrResponse.CodingKeys   FlickrResponse.Status   HTTPMethod   NetworkError   NetworkTask   Photo   PhotoStringURLBuilder   Photos   PhotosPage   PhotosPage.CodingKeys   RequestBuilder   SearchPhotosRequest  
